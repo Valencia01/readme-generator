231d7909998f6bdcf34214f27b8bb305ece03ef3
Valencia01 <kvalencia.99440@gmail.com>
2023-05-30 16:32:22 -0700

    Added a readme template and added the prompt questions

--- index.js
+++ index.js
@@ -1,13 +1,88 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+const { writeFile } = require('fs').promises;
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title to your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a small description of what your project is about.',
+      },
+      {
+        type: 'checkbox',
+        name: 'dependencies',
+        message: 'Check every compatible system.',
+        choices: ['macOS', 'Windows 10', 'Linux'],
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter details about how to install your program.',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter details about how to use your program.',
+      },
+      {
+        type: 'input',
+        name: 'credits',
+        message: 'Input every author.',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Enter a the license you are using for your project.',
+        choices: [
+            'Apache v2.0', 
+            'GNU v3.0', 
+            'MIT', 
+            'BSD 2-Clause', 
+            'BSD 3-Clause', 
+            'Boost Software v1.0', 
+            'Creative Commons Zero v1.0', 
+            'Eclipse Public v2.0',
+            'GNU v3.0',
+            'GNU v2.0',
+            'GNU v2.1',
+            'Mozilla v2.0',
+        ],
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'If you want to allow contributors to your project explain how otherwise type N/A.',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username.',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email as a point of contact.',
+      },
+    ]);
+  };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser()
+    
+}
 
 // Function call to initialize app
 init();
---
+++ template.md
@@ -0,0 +1,48 @@
+# Title
+
+## Description
+
+
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Dependencies](#dependencies)
+- [Credits](#credits)
+- [License](#license)
+- [Contribution](#contribution)
+- [Questions](#questions)
+
+## Dependencies
+
+
+
+## Installation
+
+
+
+## Usage
+
+
+
+## Credits
+
+
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details 
+
+![badge](https://img.shields.io/badge/License-MIT-blue)
+
+## Contribution
+
+
+
+## Questions
+Contact me for any questions with the following:
+- [Github](https://github.com/)
+- [example@email.com]()
+
+
